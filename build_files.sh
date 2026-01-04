16dcee93a03e3272d6f0f876f285efd448a5a843
BugHunter32 <mokshprithiani@gmail.com>
2026-01-04 23:35:39 +0500

    hello

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,7 @@
 echo "Building project..."
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
+
+# Run database migrations
+echo "Running database migrations..."
+python3.12 manage.py migrate
